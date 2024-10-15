fef2f36d888791ee9a62ab8ab0d7ff654955905e
Ryan <rtdegen579@gmail.com>
2024-10-14 18:05:56 -0600

    Add files via upload

--- assets/js/navButtons.js
+++ assets/js/navButtons.js
@@ -142,7 +142,7 @@ window.onscroll = () => {
 };
 
 function scrollFunction() {
-  if (document.body.scrollTop > 160 || document.documentElement.scrollTop > 160) {
+  if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
 	previousButton.style.display = "flex";
 	myButton.style.display = "flex";
 	forwardButton.style.display = "flex";
